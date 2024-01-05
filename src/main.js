04b15194c21036fe79bd8e6ea2c6375df519631f
emman-pip <parrenoemmanuel756@gmail.com>
2024-01-05 17:46:51 +0800

    Add the gui add subjects. (real functionality is still to be added).

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const time = (id) => {
     }
     for (let j = 0; j <= 6; j++, v++) {
       const option = document.createElement("option");
-      option.value = v;
+      option.value = v + time;
       option.text = v + " " + time;
       target.appendChild(option);
     }
@@ -25,25 +25,32 @@ async function read() {
   return data;
 }
 
+//
+
+const actionPerformedPopulate = (target, data, subjectContainer) => {
+  subjectContainer.innerHTML = "";
+  let year = target.value;
+  for (let i = 0; i < data[year].length; i++) {
+    const option = document.createElement("option");
+    option.value = data[year][i];
+    option.text = data[year][i];
+    subjectContainer.appendChild(option);
+  }
+};
 async function populateSubjects() {
   const data = await read();
-  console.log(data);
-  let year;
-  const subjectContainer = document.querySelector("#subject");
+  // let year;
+  const subjectContainer = await document.querySelector("#subject");
   const target = document.querySelector("#yearlevel");
+
+  actionPerformedPopulate(target, data, subjectContainer);
+
   target.addEventListener("click", (e) => {
-    subjectContainer.innerHTML = "";
-    year = target.value;
-    console.log(year);
-    for (let i = 0; i < data[year].length; i++) {
-      const option = document.createElement("option");
-      option.value = data[year][i];
-      option.text = data[year][i];
-      subjectContainer.appendChild(option);
-    }
+    actionPerformedPopulate(target, data, subjectContainer);
   });
 }
-
+// do the start end(will be -1)
+// then do the storage struff
 const fillSpace = (
   day,
   subject = null,
@@ -56,27 +63,39 @@ const fillSpace = (
 
   let i = 1;
   target.forEach((e) => {
-    if (e.textContent != "") {
-      // fix a this condition kasi nagiging uneditable yung shits
-      return;
+    // console.log("start:", start, "end", end);
+    if (i >= start && i <= end - 1) {
+      e.textContent = subject;
+      e.style.color = "red";
+      // e.style.outline = "none";
     }
-    e.textContent = subject;
-    e.style.color = "red";
-    // e.style.outline = "none";
     i++;
   });
+};
+
+const timeConverter = (time) => {
+  "lol".length()(time.includes("AM"));
 };
 
 const addSched = () => {
   const data = {}; // add yung data sa isang global variable
   const btn = document.querySelector(".addSubject");
   const day = document.querySelector("#day");
+  const start = document.querySelector("#start");
+  const end = document.querySelector("#end");
   const subject = document.querySelector("#subject");
   btn.addEventListener("click", (e) => {
     data["day"] = day.value;
     data["subject"] = subject.value;
+    data["start"] = start.value.includes("AM")
+      ? start.value.substring(0, start.value.length - 2) - 5
+      : parseInt(start.value.substring(0, start.value.length - 2)) + 7;
+    data["end"] = end.value.includes("AM")
+      ? parseInt(start.value.substring(0, end.value.length - 2)) - 5
+      : parseInt(end.value.substring(0, end.value.length - 2)) + 7;
+
     console.log(data);
-    fillSpace(day.value, subject.value);
+    fillSpace(day.value, subject.value, data["start"], data["end"]);
   });
 };
 
@@ -84,6 +103,6 @@ const addSched = () => {
   time("#start");
   time("#end");
   read();
-  populateSubjects();
   addSched();
+  populateSubjects();
 })();
